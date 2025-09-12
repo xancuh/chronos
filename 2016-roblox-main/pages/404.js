ae3380df473c6d319335301761e9e1e98932d849
700 <132393158+xancuh@users.noreply.github.com>
2025-09-12 21:13:04 +0100

    initial commit

---
+++ 2016-roblox-main/pages/404.js
@@ -0,0 +1,10 @@
+// pages/404.js
+export default function NotFoundPage() {
+  return (
+    <div className='row'>
+      <div className='col-6 mx-auto'>
+        <p>Page not found</p>
+      </div>
+    </div>
+  );
+}
---
+++ admin/src/components/templates/Footer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const year = new Date().getFullYear();
+</script>
+
+<footer class="footer mt-auto py-3 bg-light">
+	<div class="container">
+		<p class="text-muted mb-0 mt-2">
+			{window.location.hostname}
+		</p>
+	</div>
+</footer>
